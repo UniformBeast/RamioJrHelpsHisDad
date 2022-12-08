98d6a5d71f21806c97392c0acfbda380e9cb774d
Tome <Crash09yt@outlook.com>
2022-12-08 13:27:33 -0800

    Levels
    Made Level Zero, Made 3 folders for each subset of the four levels Modified whip to fit better with sprite Added Portal

--- Assets/Scripts/NextLevel.cs
+++ Assets/Scripts/NextLevel.cs
@@ -9,8 +9,17 @@ public class NextLevel : MonoBehaviour
     // Start is called before the first frame update
     private void OnCollisionEnter2D(Collision2D collision)
     {
-
-        SceneManager.LoadScene("SecondScene");
-                
+        if(collision.gameObject.CompareTag("A1"))
+        {
+            SceneManager.LoadScene("Level1A");
+        }
+        if (collision.gameObject.CompareTag("A2"))
+        {
+            SceneManager.LoadScene("Level2A");
+        }
+        if (collision.gameObject.CompareTag("A3"))
+        {
+            SceneManager.LoadScene("Level3A");
+        }
     }
 }
