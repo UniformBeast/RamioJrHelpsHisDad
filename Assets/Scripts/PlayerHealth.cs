16ea950539c651cc1149e9e4c4edfdae09b5bf20
Tome <Crash09yt@outlook.com>
2022-12-09 12:55:46 -0800

    Player ehelth

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+public class PlayerHealth : MonoBehaviour
+{
+    public int health = 10;
+    public Slider slider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        slider.maxValue = health;
+        slider.value = health;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            health--;
+            slider.value = health;
+        }
+    }
+}
